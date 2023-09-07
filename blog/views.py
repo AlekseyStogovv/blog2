a29a554d1ca3a0710e99621ac1ba29d8da464914
AlekseyStogovv <astogovv@gmail.com>
2023-09-07 17:56:58 +0300

    add taggs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
+from taggit.models import Tag
+
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
@@ -35,17 +37,22 @@ def post_share(request, post_id):
                                                     'sent': sent})
 
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {'posts': posts})
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts,
+                                                   'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
