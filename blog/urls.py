5f64f549df79f0d1741add0bc201a3d88f2f3a73
AlekseyStogovv <astogovv@gmail.com>
2023-09-08 23:59:24 +0300

    add search on site change database

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+from .feeds import LatestPostsFeed
+
 app_name = 'blog'
 
 urlpatterns = [
@@ -12,4 +14,6 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
